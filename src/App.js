2f634a50445dd5bc7d282ff7b98b211b1157b561
rmamchyk <roman.mamchyk@gmail.com>
2019-02-15 21:20:39 +0200

    Added base components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/Burgerbuilder';
 
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
