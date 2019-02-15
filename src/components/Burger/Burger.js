7b7eef10b650806f42881e044200e1053c4fa944
rmamchyk <roman.mamchyk@gmail.com>
2019-02-16 00:10:44 +0200

    Implemented BuildControls component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])].map((_, i) => {
+                return <BurgerIngredient key={key+i} type={key} />
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+        
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
